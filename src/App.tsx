816d9fe6e7c1fbda8f5e1e0b5b5efa410227e90e
hafidhaVoideu <hafidha@bouabdelli.com>
2023-10-18 03:06:40 +0100

    draft2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-import { TypingText } from "./components/CustomText";
 import Footer from "./scenes/global/Footer";
 import Header from "./scenes/global/Header";
 import About from "./scenes/sections/About";
@@ -7,12 +5,10 @@ import Home from "./scenes/sections/Home";
 import Members from "./scenes/sections/Members";
 import Partners from "./scenes/sections/Partners";
 import Services from "./scenes/sections/Services";
-import { staggerContainer } from "./utils/motion";
 
 function App() {
   return (
     <div className="main">
-
       <Header />
       <Home />
       <Services />
